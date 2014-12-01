5e99843d0a7ca92e260f7eecec8561f1b3d54c81
James Gill <jgill@expectlabs.com>
2014-12-01 12:03:12 -0500

    Fix #4.  Updating package.js to new package paradigm.

--- package.js
+++ package.js
@@ -1,24 +1,26 @@
 Package.describe({
-  summary: "A logger for Meteor inspired by log4j and commons-logging."
+  summary: "A logger for Meteor inspired by log4j and commons-logging.",
+  version: "0.0.6",
+  name: "jag:pince",
+  git: "https://github.com/mad-eye/pince.git",
 });
 
 Npm.depends({
-  "coffee-script": "1.6.3",
-  "underscore": "1.5.2",
   "moment": "2.4.0",
   "cli-color": "0.2.3"
 
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
+  api.versionsFrom('0.9.0');
 
-  api.use(["coffeescript", 'underscore'], ["client", "server"]);
+  api.use('underscore');
+  api.use('coffeescript');
 
   api.add_files(["src/microevent.coffee"], 'client');
-  api.add_files(['src/console.coffee', "src/logger.coffee"], ["client", 'server']);
+  api.add_files(['src/console.coffee', "src/logger.coffee"]);
 
-  if (api.export) //compat with pre-0.6.5
-    api.export("Pince", ["server", "client"]);
-    api.export("Logger", ["server", "client"]);
-    api.export("MicroEvent", ["client"]);
+  api.export("Pince");
+  api.export("Logger");
+  api.export("MicroEvent", "client");
 });
